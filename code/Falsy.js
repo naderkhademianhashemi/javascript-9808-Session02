e2ee6100f40cc19d4b4441c7e6db3bfd76c3efc5
nader <nhashemi6@gmail.com>
2023-05-20 21:08:27 +0330

    Add project files.

---
+++ code/Falsy.js
@@ -0,0 +1,11 @@
+//https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+if (false)
+if (null)
+if (undefined)
+if (0)
+if (NaN)
+if ('')
+if ("")
+if (``)
+if (document.all)
---
+++ code/truthy.js
@@ -0,0 +1,13 @@
+//https://developer.mozilla.org/en-US/docs/Glossary/Truthy
+
+if (true)
+if ({})
+if ([])
+if (42)
+if ("foo")
+if (new Date())
+if (-42)
+if (3.14)
+if (-3.14)
+if (Infinity)
+if (-Infinity)
